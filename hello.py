d62ac6a481fedc8ca3def384ae4d0e3e944673cc
Dengin Diken <d.dikennn@gmail.com>
2024-05-21 21:52:37 +0300

    Update Hello.py

--- hello.py
+++ hello.py
@@ -1,31 +1,26 @@
 import psycopg2
 from flask import Flask
-
-# Veritabanı bağlantısı kur
-host = "final-rg.postgres.database.azure.com"
-dbname = "postgres"
-user = "dengin"
-password = "Diken1234"
-sslmode = "require"
-port = 5432
-# Construct connection string
-conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4} port={5}".format(host, user, dbname, password, sslmode,port)
-
-try:
-    conn = psycopg2.connect(conn_string)
-    print("Connection established")
-    cursor = conn.cursor()
-except TypeError as e:
-    print(f"An error occurred: {e}")
-    
-    
+        
 app = Flask(__name__)    
+    
+DBHOST = 'final-rg.postgres.database.azure.com'
+DBNAME = 'postgres'
+DB_USER = 'dengin'
+DB_PASSWORD = 'Diken1234'
+
+def get_connection():
+    return psycopg2.connect(host=DBHOST, dbname=DBNAME, user=DB_USER, password=DB_PASSWORD)
 
 @app.route('/hello')
 def hello():
-    return "Hello, our my Colleagues!"
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT 'Hello, World!'")
+    result = cursor.fetchone()[0]
+    cursor.close()
+    conn.close()
+    return result
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
-    
     
