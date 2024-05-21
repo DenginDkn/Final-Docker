931f2816edb1340132c0d36bea21ffea344d1af2
Dengin Diken <d.dikennn@gmail.com>
2024-05-21 21:26:13 +0300

    Hello World Feature Branch

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import psycopg2
-from psycopg2 import OperationalError
+from flask import Flask
 
 # Veritabanı bağlantısı kur
 host = "final-rg.postgres.database.azure.com"
@@ -7,19 +7,25 @@ dbname = "postgres"
 user = "dengin"
 password = "Diken1234"
 sslmode = "require"
-
+port = 5432
 # Construct connection string
-conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4} port={5}".format(host, user, dbname, password, sslmode,port)
 
 try:
     conn = psycopg2.connect(conn_string)
     print("Connection established")
     cursor = conn.cursor()
-except OperationalError as e:
+except ConnectionError as e:
     print(f"An error occurred: {e}")
     
-def main():
-    print("Hello world")
+    
+app = Flask(__name__)    
+
+@app.route('/hello')
+def hello():
+    return "Hello, our my Colleagues!"
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
+    
+    
