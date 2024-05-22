7adca71f63ae369d655a593f9f8343d833473366
Dengin Diken <d.dikennn@gmail.com>
2024-05-22 11:36:03 +0300

    Dockerfile Updated and renew hello file

--- hello.py
+++ hello.py
@@ -1,23 +1,4 @@
-import psycopg2
 from flask import Flask
-
-# Veritabanı bağlantısı kur
-host = "final-rg.postgres.database.azure.com"
-dbname = "postgres"
-user = "dengin"
-password = "Diken1234"
-sslmode = "require"
-port = 5432
-# Construct connection string
-conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4} port={5}".format(host, user, dbname, password, sslmode,port)
-
-try:
-    conn = psycopg2.connect(conn_string)
-    print("Connection established")
-    cursor = conn.cursor()
-except TypeError as e:
-    print(f"An error occurred: {e}")
-    
     
 app = Flask(__name__)    
 
